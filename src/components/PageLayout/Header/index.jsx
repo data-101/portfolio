3e8decf2cf7ceb78cc051d6bb66776d77f9ddb03
unknown <akashnainani9@gmail.com>
2020-10-09 16:24:54 +0530

    setup

--- src/components/PageLayout/Header/index.jsx
+++ src/components/PageLayout/Header/index.jsx
@@ -1,58 +1,88 @@
-import React, { useState } from 'react';
-import { Link } from 'gatsby';
-import { Layout } from 'antd';
-import 'font-awesome/less/font-awesome.less';
-import style from './header.module.less';
-import '../../../styles/global.less';
-import { useWindowSize } from '../../../utils/hooks';
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import { Layout } from "antd"
+import "font-awesome/less/font-awesome.less"
+import style from "./header.module.less"
+import "../../../styles/global.less"
+import { useWindowSize } from "../../../utils/hooks"
 
 export default () => {
-  const [menu, setMenu] = useState(false);
+  const [menu, setMenu] = useState(false)
 
-  const [width] = useWindowSize();
+  const [width] = useWindowSize()
   const toggleMenu = () => {
     if (width !== 0 && width <= 768) {
       if (menu) {
-        setMenu(false);
+        setMenu(false)
       } else {
-        setMenu(true);
+        setMenu(true)
       }
     }
-  };
+  }
   return (
     <>
-      <div className={style.circleMenu} role="button" tabIndex="0" onKeyDown={toggleMenu} onClick={toggleMenu}>
+      <div
+        className={style.circleMenu}
+        role="button"
+        tabIndex="0"
+        onKeyDown={toggleMenu}
+        onClick={toggleMenu}
+      >
         <div className={`${style.hamburger} ${menu ? style.menuIcon : null}`}>
           <div className={style.line} />
           <div className={style.line} />
           <div className={style.hamburgerText}>MENU</div>
         </div>
       </div>
-      <Layout className={`${style.navWrap} ${menu ? null : style.hidden} ${menu ? style.openMenu : null}`}>
+      <Layout
+        className={`${style.navWrap} ${menu ? null : style.hidden} ${
+          menu ? style.openMenu : null
+        }`}
+      >
         <div className={style.backgroundDiv}>
           <ul className={style.nav}>
             <li className={style.navItem}>
-              <Link to="/" onClick={toggleMenu} activeClassName={style.anchorActive}>
+              <Link
+                to="/"
+                onClick={toggleMenu}
+                activeClassName={style.anchorActive}
+              >
                 About
               </Link>
             </li>
-            <li className={style.navItem}>
-              <Link to="/contact" onClick={toggleMenu} activeClassName={style.anchorActive}>
+            {/* <li className={style.navItem}>
+              <Link
+                to="/contact"
+                onClick={toggleMenu}
+                activeClassName={style.anchorActive}
+              >
                 Contact
               </Link>
-            </li>
-            <li className={style.navItem}>
-              <Link to="/blog" onClick={toggleMenu} activeClassName={style.anchorActive}>
+            </li> */}
+            {/* <li className={style.navItem}>
+              <Link
+                to="/blog"
+                onClick={toggleMenu}
+                activeClassName={style.anchorActive}
+              >
                 Blog
               </Link>
             </li>
             <li className={style.navItem}>
-              <Link to="/tags" onClick={toggleMenu} activeClassName={style.anchorActive}>
+              <Link
+                to="/tags"
+                onClick={toggleMenu}
+                activeClassName={style.anchorActive}
+              >
                 Tags
               </Link>
-            </li>
+            </li> */}
             <li className={style.navItem}>
-              <Link to="/resume" onClick={toggleMenu} activeClassName={style.anchorActive}>
+              <Link
+                to="/resume"
+                onClick={toggleMenu}
+                activeClassName={style.anchorActive}
+              >
                 Resume
               </Link>
             </li>
@@ -60,5 +90,5 @@ export default () => {
         </div>
       </Layout>
     </>
-  );
-};
+  )
+}
